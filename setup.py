3e9a699ff02d2e0d31975d4072b82e2d3a871506
panakouris <ppkouris@gmail.com>
2019-10-23 13:36:22 +0200

    Setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='altimetrydb',
+    version='v0.1.0',
+    description='Query and download data from CreoDias API',
+    author='Pantelis Kouris',
+    author_email='pako@dhigroup.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'six',
+        'tqdm',
+        'python-dateutil'
+    ]
+)
