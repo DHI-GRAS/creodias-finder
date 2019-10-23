b7a5886fc42106b36c51b54d2957722149160cdf
panakouris <ppkouris@gmail.com>
2019-10-23 13:24:07 +0200

    :lipstick:

--- creodias_finder/download.py
+++ creodias_finder/download.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 
 pbar = tqdm()
 
+
 def download(uid, username, password, outfile=None, workdir=None):
     """Downloads a file to the given location.
     This function stores unfinished downloads in the given working directory.
@@ -22,14 +23,14 @@ def download(uid, username, password, outfile=None, workdir=None):
         :param workdir: Path where incomplete downloads are stored.
         :param outfile: Output filename.
     """
-    if outfile==None:
-        outfile=f'{uid}.zip'
+    if not outfile:
+        outfile = f'{uid}.zip'
 
     token_data = {
-    'client_id': 'CLOUDFERRO_PUBLIC',
-    'username': username,
-    'password': password,
-    'grant_type': 'password'
+        'client_id': 'CLOUDFERRO_PUBLIC',
+        'username': username,
+        'password': password,
+        'grant_type': 'password'
     }
     url = f'https://zipper.creodias.eu/download/{uid}'
 
@@ -44,9 +45,10 @@ def download(uid, username, password, outfile=None, workdir=None):
     local_path = temp.name
 
     url += f'?token={token}'
-    #with requests.get(url, stream=True, headers={'token': token}) as resp:
+    # with requests.get(url, stream=True, headers={'token': token}) as resp:
     _download_raw_data(url, local_path)
     shutil.move(local_path, outfile)
+
 
 def download_list(uids, username, password, outdir=None, workdir=None, threads=3):
     """Downloads a list of UIDS
@@ -59,8 +61,8 @@ def download_list(uids, username, password, outdir=None, workdir=None, threads=3
     :param threads: Number of simultaneous downloads.
     """
     pool = ThreadPool(threads)
-    #for uid in uids:
-    download_lambda = lambda x : download(x, username, password, outfile=None, workdir=None)
+    # for uid in uids:
+    download_lambda = lambda x: download(x, username, password, outfile=None, workdir=None)
     pool.map(download_lambda, uids)
         #threading.Thread(target=download, args=)
         #download(uid, username, password, outdir)
@@ -73,7 +75,7 @@ def _download_raw_data(url, path):
     with closing(
             requests.get(url, stream=True, timeout=10)
     ) as r, tqdm(
-        unit = 'B',
+        unit='B',
         unit_scale=True
     ) as progress:
         chunk_size = 2 ** 20  # download in 1 MB chunks
@@ -87,17 +89,3 @@ def _download_raw_data(url, path):
                     i += 1
         # Return the number of bytes downloaded
         return downloaded_bytes
-
-
-
-if __name__ == "__main__":
-    # uid = 'c0e762e2-fb27-5f9e-9cd0-7883d99c3b81'#'db0c8ef3-8ec0-5185-a537-812dad3c58f8'
-    # download(uid, outfile="test.zip", username='anbr@dhigroup.com', password='aCAtX.D.-qn9vnkGRFDm')
-
-    uids = ['c0e762e2-fb27-5f9e-9cd0-7883d99c3b81',
-            'dfe2b8a6-6b95-5f47-8d36-44806e509d8d',
-            '4f6c38e4-a3f8-51b9-8106-4d0a4652acc7',
-            'd0e0e0b5-5008-5096-8726-63c242c63511',
-            '68959a6d-0793-5df4-8b0c-8daf49b001ba',
-            'c9f42d5b-3238-5ee9-8c2d-06b4e3046330']
-    download_list(uids, username='anbr@dhigroup.com', password='aCAtX.D.-qn9vnkGRFDm', threads=6)
