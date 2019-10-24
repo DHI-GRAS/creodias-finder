17c54b566918b7885fd4ba1e31a98d8751e7d402
anbr <aborgstad@gmail.com>
2019-10-24 15:21:40 +0200

    Add workdir fixture, parameterized

--- creodias_finder/tests/conftest.py
+++ creodias_finder/tests/conftest.py
@@ -1,18 +1,21 @@
 import os
 import pytest
-
+import uuid
+from pathlib import Path
 
 SMALL_FILES = ['e7775c05-6cd2-5e2e-9439-593415de220c',
                '87b3dba1-5eca-5b23-9dac-b7cb2d7a25c8',
                '8d560dc5-d127-559f-b4cf-4e26e5d2920b',
                'd0e0e0b5-5008-5096-8726-63c242c63511']
 
+
 @pytest.fixture
 def password():
     passw = os.environ['CREODIAS_PASSWORD']
     if passw:
         return passw
-    else: raise ValueError("Set environment variable CREODIAS_PASSWORD")
+    else:
+        raise ValueError("Set environment variable CREODIAS_PASSWORD")
 
 
 @pytest.fixture
@@ -20,7 +23,8 @@ def username():
     user = os.environ['CREODIAS_USERNAME']
     if user:
         return user
-    else: raise ValueError("Set environment variable CREODIAS_USERNAME")
+    else:
+        raise ValueError("Set environment variable CREODIAS_USERNAME")
 
 
 @pytest.fixture
@@ -32,4 +36,9 @@ def uid():
 @pytest.fixture
 def uids():
     return SMALL_FILES
+
+
+@pytest.fixture(params=[None, str(uuid.uuid1()), Path(str(uuid.uuid1()))])
+def workdir(request):
+    yield request.param
 
--- creodias_finder/tests/test_download.py
+++ creodias_finder/tests/test_download.py
@@ -1,13 +1,38 @@
 import os
 from pathlib import Path
-from creodias_finder.download import download
+from creodias_finder.download import download, download_list
 
 
-def test_download(uid, username, password, tmp_path):
-    outfile, workdir = tmp_path / "outfiles", tmp_path / "workdir"
-    outfile.mkdir()
-    workdir.mkdir()
+def test_download(uid, username, password, tmp_path, workdir):
+    #outfile, workdir = tmp_path / "outfiles", tmp_path / "workdir"
+    create_workdir_if_not_exists(workdir)
+
+    outfile = tmp_path
     download(uid, username, password, outfile=outfile, workdir=workdir)
     write_path = list(outfile.glob("*"))[0]
     assert outfile / f'{uid}.zip' == Path(write_path)
+
+def test_download_list(uids, username, password, tmp_path):
+    outfile, workdir = tmp_path / "outfiles", tmp_path / "workdir"
+    outfile.mkdir()
+    workdir.mkdir()
+    download_list(uids, username, password,
+                  outdir=outfile, workdir=workdir, threads=max(10,len(uids)))
+
+    created_files = set()
+    for p in outfile.glob("*"):
+        created_files.add(str(p))
+    file_assertions = set()
+    for uid in uids:
+        file_assertions.add(str(outfile / f'{uid}.zip'))
+
+    assert created_files == file_assertions
+
+def create_workdir_if_not_exists(p):
+    if p:
+        print('THIS IS P ', p)
+        p = Path(p)
+        if not p.is_dir():
+            p.mkdir()
+        #if not p.is_file():
 
