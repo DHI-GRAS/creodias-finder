452354a4b7c3ca91d2198380911f42e54a182c9d
anbr <aborgstad@gmail.com>
2019-10-25 10:07:48 +0200

    Added newline..

--- creodias_finder/tests/test_download.py
+++ creodias_finder/tests/test_download.py
@@ -10,6 +10,7 @@ def test_download(uid, username, password, tmp_path, workdir):
     write_path = list(outfile.glob("*"))[0]
     assert outfile / f'{uid}.zip' == Path(write_path)
 
+
 def test_download_list(uids, username, password, tmp_path):
     outfile, workdir = tmp_path / "outfiles", tmp_path / "workdir"
     outfile.mkdir()
@@ -25,6 +26,7 @@ def test_download_list(uids, username, password, tmp_path):
         file_assertions.add(str(outfile / f'{uid}.zip'))
 
     assert created_files == file_assertions
+
 
 def create_workdir_if_not_exists(path):
     if path:
