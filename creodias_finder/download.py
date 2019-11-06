6e13b43bf0aa8986020f9643ab2fa6f9bcb09749
anbr <aborgstad@gmail.com>
2019-11-06 16:26:00 +0100

    Bugfix: Disable single-dl progress bar when using multithreading

--- creodias_finder/download.py
+++ creodias_finder/download.py
@@ -23,7 +23,7 @@ def _get_token(username, password):
         raise RuntimeError(f'Unable to get token. Response was {response}')
 
 
-def download(uid, username, password, outfile):
+def download(uid, username, password, outfile, show_progress=True):
     """Download a file from CreoDIAS to the given location
 
     Parameters
@@ -39,10 +39,10 @@ def download(uid, username, password, outfile):
     """
     token = _get_token(username, password)
     url = f'{DOWNLOAD_URL}/{uid}?token={token}'
-    _download_raw_data(url, outfile)
+    _download_raw_data(url, outfile, show_progress)
 
 
-def download_list(uids, username, password, outdir, threads=1):
+def download_list(uids, username, password, outdir, threads=1, show_progress=True):
     """Downloads a list of UIDS
 
     Parameters
@@ -63,9 +63,13 @@ def download_list(uids, username, password, outdir, threads=1):
     dict
         mapping uids to paths to downloaded files
     """
+    if show_progress:
+        pbar =  tqdm(total = len(uids), unit='files')
     def _download(uid):
-        outfile = Path(outdir) / '{uuid}.zip'
-        download(uid, username, password, outfile=outfile)
+        outfile = Path(outdir) / f'{uid}.zip'
+        download(uid, username, password, outfile=outfile, show_progress=False)
+        if show_progress:
+            pbar.update(1)
         return uid, outfile
 
     with concurrent.futures.ThreadPoolExecutor(threads) as executor:
@@ -74,13 +78,13 @@ def download_list(uids, username, password, outdir, threads=1):
     return paths
 
 
-def _download_raw_data(url, outfile):
+def _download_raw_data(url, outfile, show_progress):
     """Downloads data from url to outfile.incomplete and then moves to outfile"""
     outfile_temp = str(outfile) + '.incomplete'
     try:
         downloaded_bytes = 0
         with requests.get(url, stream=True, timeout=10) as req:
-            with tqdm(unit='B', unit_scale=True) as progress:
+            with tqdm(unit='B', unit_scale=True, disable=not show_progress) as progress:
                 chunk_size = 2 ** 20  # download in 1 MB chunks
                 with open(outfile_temp, 'wb') as fout:
                     for chunk in req.iter_content(chunk_size=chunk_size):
