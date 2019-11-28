9c3f14f0bda4421862e48c77221e03cc429a51aa
panakouris <ppkouris@gmail.com>
2019-11-28 13:22:10 +0100

    Revert "Update creodias_storage.py"
    This reverts commit e9cb8f38bbd96103bc59b1663bad6f50639de88b.

--- creodias_finder/creodias_storage.py
+++ creodias_finder/creodias_storage.py
@@ -95,8 +95,6 @@ class S3Storage:
 
         for item in files:
             dest.joinpath(item).parent.mkdir(parents=True, exist_ok=True)
-            print(pkey)
-            print(item)
             print(str(pkey.joinpath(item)))
             self.s3_client.download_file(bucket, str(
                 pkey.joinpath(item)), str(dest.joinpath(item)))
