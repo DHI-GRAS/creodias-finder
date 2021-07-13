0ca6f1d98ea5b83c7e274ac12ceb5e463503ff61
Jonas <j08lue@gmail.com>
2021-07-13 16:19:58 +0200

    Update test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,17 +2,17 @@ from pathlib import Path
 from creodias_finder.download import download, download_list
 
 
-def test_download(uid, username, password, tmp_path, workdir):
-    create_workdir_if_not_exists(workdir)
-
-    outfile = tmp_path
-    download(uid, username, password, outfile=outfile, workdir=workdir)
-    write_path = list(outfile.glob("*"))[0]
-    assert outfile / f"{uid}.zip" == Path(write_path)
+def test_download(uid, username, password, tmp_path):
+    workdir = tmp_path / "workdir"
+    workdir.mkdir()
+    download(uid, username, password, outfile=tmp_path, workdir=workdir)
+    expected_path = Path(list(tmp_path.glob("*"))[0])
+    assert tmp_path / f"{uid}.zip" == expected_path
 
 
 def test_download_list(uids, username, password, tmp_path):
-    outfile, workdir = tmp_path / "outfiles", tmp_path / "workdir"
+    outfile = tmp_path / "outfiles"
+    workdir = tmp_path / "workdir"
     outfile.mkdir()
     workdir.mkdir()
     download_list(
@@ -32,11 +32,3 @@ def test_download_list(uids, username, password, tmp_path):
         file_assertions.add(str(outfile / f"{uid}.zip"))
 
     assert created_files == file_assertions
-
-
-def create_workdir_if_not_exists(path):
-    if path:
-        print("THIS IS P ", path)
-        path = Path(path)
-        if not path.is_dir():
-            path.mkdir()
